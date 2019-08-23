f19da3ace8ce2456a66621d8c3da5f612055af25
erferguson-44 <ferguson.eric.r@gmail.com>
2019-08-23 09:00:50 -0700

    OBJECTS -- all q's answered

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,37 +12,40 @@ const tyrannosaurus = {
   weight: "7000kg",
   length: "12m",
   period: "Late Cretaceous"
-}
+};
 const stegosaurus = {
   name: "Stegosaurus",
   diet: "herbivorous",
   weight: "2000kg",
   length: "9m",
   period: "Late Jurassic"
-}
+};
 const velociraptor = {
   name: "Velociraptor",
   diet: "carnivorous",
   weight: "15kg",
   length: "1.8m",
   period: "Late Cretaceous"
-}
+};
 
 // How much did tyrannosaurus weigh?
 console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tyrannosaurus.roar = function(){
+  return `${tyrannosaurus.name} says "RAWERSRARARWERSARARARRRR!"`;
+}
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
